69b8c678d550e2e5fc8ae10fd4b0cfbec6c55d3b
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-03 20:53:01 +0200

    feat( add longNames array with names >= 5 characters)

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,6 +15,8 @@ console.log('Reversed:', reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+const longNames = teachers.filter(name => name.length >= 5);
+console.log ('long Names', longNames)
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
