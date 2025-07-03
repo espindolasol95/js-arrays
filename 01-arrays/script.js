e3abde59e953f43b21bf5eb7e0630430096bc12a
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-03 18:27:02 +0200

    feat(Check if the teachers array is empty)

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -42,7 +42,8 @@ console.log('Index of Lewis:', lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty =  teachers.length ===0;
+console.log('Is teachers array empty?', isTeachersEmpty); //(questo esercizio è stato fatto senza comprenderlo del tutto)
 
 
 
