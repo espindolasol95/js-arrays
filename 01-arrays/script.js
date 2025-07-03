6e765ad0357e3e67143f60e4efde41893a040116
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-03 17:54:53 +0200

    feat(Remove the last teacher and save it in lastTeacher)

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,14 +11,17 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers [3];
-console.log('four_teacher:', fourthTeacher);
+console.log('four teacher:', fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers [4] = 'Patrick' ;
-console.log('last_teacher:',teachers );
+console.log('Updated teachers after replacing 5th teacher:', teachers);
+
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log('Last teacher removed:', lastTeacher);
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
